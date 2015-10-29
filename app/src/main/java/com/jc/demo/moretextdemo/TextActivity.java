9d0f54960d1a6dec5304060d0dc706d00427e1ec
chenzujie <chenzujie@91yong.com>
2015-10-29 11:51:09 +0800

    文案修改，提交gif

--- app/src/main/java/com/jc/demo/moretextdemo/TextActivity.java
+++ app/src/main/java/com/jc/demo/moretextdemo/TextActivity.java
@@ -9,16 +9,13 @@ public class TextActivity extends Activity {
 
     private TextView mTvMore = null;
 
-    private String mText = "设置TextView 显示更多\n" +
-            "    使用方法：\n" +
-            "     textview.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {\n" +
-            "     @Override\n" +
-            "     public boolean onPreDraw() {\n" +
-            "     textview.getViewTreeObserver().removeOnPreDrawListener(this);\n" +
-            "     TextUtil.setEllipsize(...)\n" +
-            "     return true;\n" +
-            "     }\n" +
-            "     });";
+    private String mText = "Android（读音：英 ['ændrɔɪd] 美 [ˈænˌdrɔɪd]），" +
+            "中文俗称安卓，是一个以Linux为基础的开放源代码移动设备操作系统，主要用于移动设备，" +
+            "由Google成立的Open Handset Alliance（OHA，开放手持设备联盟）持续领导与开发中。" +
+            "Android系统最初由安迪·鲁宾（Andy Rubin）等人开发制作，" +
+            "最初开发这个系统的目的是创建一个数码相机的先进操作系统；" +
+            "但是后来发现市场需求不够大，加上智能手机市场快速成长，" +
+            "于是Android被改造为一款面向智能手机的操作系统。";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
